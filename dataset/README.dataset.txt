fcb417b88f4035b43ddba7f8db9f5a8bd6f0cdea
Vignesh <naanvicky14@gmail.com>
2026-09-04 20:12:58 +0530

    Initial RetailAI YOLO project

---
+++ dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# POTAZOS > 2026-09-04 5:12pm
+https://universe.roboflow.com/vignesh-raj-m/potazos
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import cv2
+
+person_model = YOLO("yolo11s.pt")
+potazos_model = YOLO(r"V:\RetailAI\runs\detect\train\weights\best.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Run BOTH models on the original clean frame
+    person_results = person_model(frame, conf=0.50, verbose=False)
+    potazos_results = potazos_model(frame, conf=0.70, verbose=False)
+
+    # Draw person detections
+    output = person_results[0].plot()
+
+    # Draw Potazos detections
+    output = potazos_results[0].plot(img=output)
+
+    cv2.imshow("RetailAI - Person + Potazos", output)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
